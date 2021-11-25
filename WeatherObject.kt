d09088fb6cc91e969a22d8e2fede7879d9ded8b2
BrazhnikDA <65236303+BrazhnikDA@users.noreply.github.com>
2021-11-26 01:16:38 +0300

    Add Structured output to the console. Changing some values from a string to a double

--- WeatherObject.kt
+++ WeatherObject.kt
@@ -1,19 +1,20 @@
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class WeatherObject(
     name_: String,
     weather_: String,
-    main_: String,
+    main_: Double,
     description_: String,
-    feelsLike_: String,
-    speed_: String
+    feelsLike_: Double,
+    speed_: Double
 ) {
     private var name: String = name_
     private var weather: String = weather_
-    private var main: String = main_
+    private var main: Double = main_
     private var description: String = description_
-    private var feelsLike: String = feelsLike_
-    private var speed: String = speed_
+    private var feelsLike: Double = feelsLike_
+    private var speed: Double = speed_
 
     init {
         name = name_
@@ -25,13 +26,31 @@ class WeatherObject(
     }
 
     override fun toString(): String {
-        
-        return "City:$name\n" +
-                "Temperature:\t$main°\n" +
-                "Feels like:\t$feelsLike°\n" +
-                "Weather:\t$weather\n" +
-                "Description:\t$description\n" +
-                "Wind speed:\t$speed\n" +
-                "Date: ${LocalDateTime.now()}\n"
+        val outString: StringBuilder = StringBuilder()
+
+        val formattedStringTitle = "| %-10s"
+        val formattedValueString = "%-18s |\n"
+        val formattedValueDouble = "%-18.1f |\n"
+
+        val cityTitleStr = "City:"
+        val tempTitleStr = "Temp.:"
+        val feelsTitleStr = "Feels:"
+        val weatherTitleStr = "Weather:"
+        val conditionTitleStr = "Status:"
+        val windTitleStr = "Wind:"
+        val dateTitleStr = "Date:"
+        val dateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("M/d/y H:m:ss"))
+
+        outString.append("- - - - - - - - - - - - - - - -\n")
+        outString.append(formattedStringTitle.format(cityTitleStr) + formattedValueString.format(name))
+        outString.append(formattedStringTitle.format(tempTitleStr) + formattedValueDouble.format(main))
+        outString.append(formattedStringTitle.format(feelsTitleStr) + formattedValueDouble.format(feelsLike))
+        outString.append(formattedStringTitle.format(weatherTitleStr) + formattedValueString.format(weather))
+        outString.append(formattedStringTitle.format(conditionTitleStr) + formattedValueString.format(description))
+        outString.append(formattedStringTitle.format(windTitleStr) + formattedValueDouble.format(speed))
+        outString.append(formattedStringTitle.format(dateTitleStr) + formattedValueString.format(dateTime))
+        outString.append("- - - - - - - - - - - - - - - -\n")
+
+        return outString.toString()
     }
 }
